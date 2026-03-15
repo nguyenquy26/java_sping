b3d4d53513719a2ffdc3964aacdeb7eebfa82785
nguyenquy26 <nguyenquy2602nd@gmail.com>
2026-03-15 11:48:59 +0700

    IoC and DI

---
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -0,0 +1,22 @@
+package vn.hoidanit.laptopshop;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World Update";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin page";
+    }
+}
