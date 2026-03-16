44223c07c2dd8cf685b9ca8bc91a1e5fd17ba97a
nguyenquy26 <nguyenquy2602nd@gmail.com>
2026-03-16 15:43:28 +0700

    config view engi witd jsp : 1.webconfig 2.applitation

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -7,15 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
 
-// @Controller
-// public class UserController {
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "Hello from controller";
-//     }
-// }
-
-@RestController
+@Controller
 public class UserController {
     // DI : dependency injection
     private UserService userService;
@@ -24,8 +16,25 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("")
+    @RequestMapping("/duonglink")
     public String getHomePage() {
-        return userService.handleHello();
+        String teString = userService.handleHello();
+
+        return "hello";
     }
 }
+
+// @RestController
+// public class UserController {
+// // DI : dependency injection
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// public String getHomePage() {
+// return userService.handleHello();
+// }
+// }
